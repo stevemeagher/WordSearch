4845e0336bb753b2c8aaefba0bbea0dc25e17fe9
Steve Meagher <stevemeagher@gmail.com>
2018-12-19 22:28:13 -0500

    added test for creating index-to-grid dictionary for top-right to bottom-left strategy

--- Tests/GridToLinearStrategyTests.cs
+++ Tests/GridToLinearStrategyTests.cs
@@ -234,6 +234,24 @@ namespace Tests
             Assert.True(expected == linearView.Value);
         }
 
+        [Fact]
+        public void GridToLinearTopRightBottomLeftStrategy_NxNStringGrid_ReturnsIndexToGridDictionary()
+        {
+            //arrange
+            string[,] grid = {
+                {"A","B"},
+                {"C","D"}
+            };
+
+            var expected = new Dictionary<int, Point>() {{0, new Point(0,0)}, {1, new Point(1,0)}, {2, new Point(0,1)}, {3, new Point(1,1)}};
+
+            //act
+            var gridToLinearStrategy = new GridToLinearTopRightBottomLeftStrategy(grid);
+            LinearView linearView = gridToLinearStrategy.GridToLinear();
+
+            //assert
+            Assert.Equal(expected, linearView.IndexToGridPosition);
+        }
 
     }
 }
