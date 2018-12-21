3c7938ce158e65e83e8ce83f0eff6387acd2a477
Steve Meagher <stevemeagher@gmail.com>
2018-12-20 22:13:44 -0500

    added test for loading lines from text file into string array

---
+++ FileLib/FileOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WordSearch.FileLib
+{
+    public class FileOperations
+    {
+        private string _applicationBasePath = "";
+        private string _baseApplicationDirectory = "";
+
+        public string[] ReadLines(string filePath)
+        {
+            return System.IO.File.ReadAllLines(filePath);
+        }
+
+        public string ApplicationBasePath(string baseApplicationDirectory)
+        {
+            if (_applicationBasePath == "" || baseApplicationDirectory != _baseApplicationDirectory)
+            {
+                _baseApplicationDirectory = baseApplicationDirectory;
+                _applicationBasePath = GetApplicationBasePath(baseApplicationDirectory);
+            }
+
+            return _applicationBasePath;
+        }
+
+        private string GetApplicationBasePath(string baseApplicationDirectory)
+        {
+            string path = System.IO.Directory.GetCurrentDirectory();
+
+            var indexOfBaseName = path.IndexOf(baseApplicationDirectory);
+
+            if (indexOfBaseName == -1)
+            {
+                throw new ApplicationException($"Applictaion path does not include {baseApplicationDirectory}");
+            }
+
+            return path.Substring(0,indexOfBaseName + baseApplicationDirectory.Length);
+        }
+    }
+}
---
+++ Tests/FileOperationsTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using WordSearch.FileLib;
+
+namespace Tests
+{
+    public class FileOperationsTests
+    {
+        [Fact]
+        public void ReadLines_WhenFileExists_ReturnsStringArrayOfFileLines()
+        {
+            //arrange
+            FileOperations fileOperations = new FileOperations();
+            string firstLine = "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA";
+            string secondLine = "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E";
+
+            //act
+            string[] fileLines = fileOperations.ReadLines(fileOperations.ApplicationBasePath("WordSearch") + "/puzzles/highlyillogical.txt");
+
+            //assert
+            Assert.True(fileLines[0] == firstLine);
+            Assert.True(fileLines[1] == secondLine);
+        }
+
+    }
+}
