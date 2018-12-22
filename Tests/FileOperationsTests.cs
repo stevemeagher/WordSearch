87fb199c59f8e472567bceba5c08d3a9aaca6363
Steve Meagher <stevemeagher@gmail.com>
2018-12-22 16:05:35 -0500

    refactored FileOperationsTest so they do not rely on existing sub-directory and files

--- Tests/FileOperationsTests.cs
+++ Tests/FileOperationsTests.cs
@@ -1,39 +1,76 @@
 using System;
+using System.IO;
 using Xunit;
 using WordSearch.FileLib;
 
 namespace Tests
 {
-    public class FileOperationsTests
+    public class FileOperationsTests : IDisposable
     {
-        [Fact]
-        public void ReadLines_WhenFileExists_ReturnsStringArrayOfFileLines()
-        {
-            //arrange
-            FileOperations fileOperations = new FileOperations();
-            string firstLine = "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA";
-            string secondLine = "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E";
+        private const string  APPLICATION_DIRECTORY = "WordSearch";
+        private string _testFilePath;
+        private FileOperations _fileOperations;
 
+        public FileOperationsTests()
+        {
+            _fileOperations = new FileOperations();
+            _testFilePath = _fileOperations.ApplicationBasePath(APPLICATION_DIRECTORY) + "testFile.txt";
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
+            string[] fileLinesOut = {"First", "Second", "Third", "Fourth", "Fifth"};
+
+            File.WriteAllLines(_testFilePath, fileLinesOut);
+        }
+
+        [Fact]
+        public void ReadLines_WhenFileExists_ReturnsArrayOfLines()
+        {
             //act
-            string[] fileLines = fileOperations.ReadLines(fileOperations.ApplicationBasePath("WordSearch") + "/puzzles/highlyillogical.txt");
+            string[] fileLines = _fileOperations.ReadLines(_testFilePath);
 
             //assert
-            Assert.True(fileLines[0] == firstLine);
-            Assert.True(fileLines[1] == secondLine);
+            Assert.True(fileLines[0] == "First");
+            Assert.True(fileLines[1] == "Second");
+            Assert.True(fileLines[2] == "Third");
+            Assert.True(fileLines[3] == "Fourth");
+            Assert.True(fileLines[4] == "Fifth");
+        }
+
+        [Fact]
+        public void ReadLines_WhenFileExists_ReturnsAllLinesInFile()
+        {
+            //act
+            string[] fileLines = _fileOperations.ReadLines(_testFilePath);
+
+            //assert
+            Assert.True(fileLines.Length == 5);
         }
 
         [Fact]
         public void ReadLines_WhenFileDoesNotExists_ReturnsEmptyArray()
         {
             //arrange
-            FileOperations fileOperations = new FileOperations();
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
 
             //act
-            string[] fileLines = fileOperations.ReadLines(fileOperations.ApplicationBasePath("WordSearch") + "/puzzles/nofile.txt");
+            string[] fileLines = _fileOperations.ReadLines(_testFilePath);
 
             //assert
             Assert.True(fileLines.Length == 0);
         }
 
+        public void Dispose()
+        {
+            //clean-up
+            if (File.Exists(_testFilePath))
+            {
+                File.Delete(_testFilePath);
+            }
+        }
     }
 }
