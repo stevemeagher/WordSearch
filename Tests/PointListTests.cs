f5c74b0f092c4ad9661af8c41dd4b895e2de6f75
Steve Meagher <stevemeagher@gmail.com>
2018-12-28 13:37:43 -0500

    added tests for PointList

---
+++ Tests/PointListTests.cs
@@ -0,0 +1,50 @@
+using System.Drawing;
+using Xunit;
+using WordSearch.WordSearchLib;
+
+namespace Tests
+{
+    public class PointListTests
+    {
+        [Fact]
+        public void ToString_WhenPointListContainsListOfPoints_ReturnsPointsAsSingleCommaDelimeteredString()
+        {
+            //arrange
+            string expected = "(0,0),(1,1),(2,2)";
+            PointList points = new PointList() {new Point(0,0), new Point(1,1), new Point(2,2)};
+
+            //act
+            string pointsAsString = points.ToString();
+
+            //assert
+            Assert.Equal(expected, pointsAsString);
+        }
+
+        [Fact]
+        public void ToString_WhenPointListInitializedWithNoPointMessageAndContainsNoPoints_ReturnsMessage()
+        {
+            //arrange
+            string expected = "NO POINTS!";
+            PointList points = new PointList(expected);
+
+            //act
+            string pointsAsString = points.ToString();
+
+            //assert
+            Assert.Equal(expected, pointsAsString);
+        }
+
+        [Fact]
+        public void ToString_WhenPointListInitializedWithoutNoPointMessageAndContainsNoPoints_ReturnsEmptyString()
+        {
+            //arrange
+            PointList points = new PointList();
+
+            //act
+            string pointsAsString = points.ToString();
+
+            //assert
+            Assert.Equal("", pointsAsString);
+        }
+    }
+}
